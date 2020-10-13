a6adb8b4a2b6e268565bd20ea3fff59692e5886b
Grayson Myers <graymyers10@gmail.com>
2020-10-13 16:44:00 -0600

    Add cells attribute and private helper method to board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,23 @@
-class Board
-  def initialize(width,height)
+require './lib/cell.rb'
 
+class Board
+  attr_reader :cells
+  def initialize(width = 4,height = 4)
+    @cells = create_empty_cells(width,height)
   end
+
+  private
+
+  def create_empty_cells(width,height)
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    cells = {}
+    width.times do |x|
+      height.times do |y|
+        coordinate = letters[y] + x.to_s
+        cells[coordinate] = Cell.new(coordinate)
+      end
+    end
+    cells
+  end
+
 end
