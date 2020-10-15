847ea58368a286909cd9fd0f8d1adcfb1e8c7d29
Grayson Myers <graymyers10@gmail.com>
2020-10-14 18:25:04 -0600

    Add test if coordinates are consecutive

--- test/board_test.rb
+++ test/board_test.rb
@@ -26,7 +26,13 @@ class BoardTest < Minitest::Test
 
   def test_that_ship_coords_are_valid
     assert_equal true, @board.valid_placement?(@cruiser,["A1","A2","A3"])
+    assert_equal true, @board.valid_placement?(@cruiser,["A1","B1","C1"])
+
     assert_equal false, @board.valid_placement?(@cruiser,["A1","A2","Z3"])
+    assert_equal false, @board.valid_placement?(@cruiser,["A1","A2","Z3"])
+    assert_equal false, @board.valid_placement?(@cruiser,["A1","A2","A3","A4"])
+    assert_equal false, @board.valid_placement?(@cruiser,["A1","D2","C3"])
+    assert_equal false, @board.valid_placement?(@cruiser,["A1","A2","B2"])
   end
 
 end
