054996dfb9ff8ea2db16bbda624b0669aa1c6931
Grayson Myers <graymyers10@gmail.com>
2020-10-20 15:32:58 -0600

    Move runner functionality into game class

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,31 +1,4 @@
-require './lib/ship'
-require './lib/board'
-require './lib/user_interface'
-require './lib/computer'
+require './lib/game'
 
-$ui = UserInterface.new
-$computer = Computer.new
-
-def prompt_play
-  option = $ui.determine_play
-  if option == :continue
-    $ui.query_custom
-    play_game
-  end
-end
-
-def play_game
-  $ui.setup
-  $computer.setup($ui.user_board, $ui.computer_board, $ui.computer_ships)
-  $computer.place_ships
-  puts $ui.prompt_ship_placement
-  $ui.determine_ship_placement
-  until winner = $ui.winner do
-    $ui.turn
-    $computer.turn
-  end
-  puts winner
-  prompt_play
-end
-
-prompt_play
+game = Game.new
+game.prompt_play
