e9ef520ad56d182e318e70bcb7b45b8e13cd72b1
Grayson Cunningham <gecun289@gmail.com>
2020-10-20 19:51:01 -0600

    Test ui methods through game setup

--- test/user_interface_test.rb
+++ test/user_interface_test.rb
@@ -2,6 +2,7 @@ require './lib/user_interface.rb'
 require './lib/computer'
 require './lib/board.rb'
 require './lib/ship.rb'
+require './lib/custom_options'
 
 require 'minitest/autorun'
 require 'minitest/pride'
@@ -15,46 +16,26 @@ class UserInterfaceTest < Minitest::Test
     @computer.setup(@ui.user_board, @ui.computer_board, @ui.computer_ships)
   end
 
+
   def test_it_exists
     assert_instance_of UserInterface, @ui
   end
 
   def test_determine_play
-    
-    @ui.stubs(:get_requested_input).returns(:continue)
-    assert_equal :continue, @ui.determine_play
-
     @ui.stubs(:get_requested_input).returns(nil)
     assert_nil @ui.determine_play
   end
 
   def query_custom
-    skip
     @ui.stubs(:get_requested_input).returns(nil)
+
     assert_nil @ui.query_custom
-  end
-
-  def test_custom_board_dimensions
-    skip
-    @ui.stubs(:get_integer).returns(5)
-    @ui.custom_board
-    @ui.setup
-
-    assert_equal 25, @ui.user_board.cells.count
-  end
-
-  def test_custom_ships
-    skip
-    @ui.stubs(:ships).returns([["Battleship", 5], ["Destroyer", 4]])
-    @ui.setup
-  require "pry"; binding.pry
-    assert_equal "Battleship", @ui.user_ships[0].name
-
-
+    assert_nil @ui.custom
   end
 
   def test_it_creates_separate_objects_for_user_and_computer
-    skip
+    @ui.setup
+
     assert_instance_of Ship, @ui.user_ships[0]
     assert_instance_of Ship, @ui.computer_ships[0]
     assert_instance_of Board, @ui.user_board
@@ -62,4 +43,15 @@ class UserInterfaceTest < Minitest::Test
     assert @ui.user_board != @ui.computer_board
     assert @ui.user_board != @computer.board
   end
+
+  def test_prompt_ship_placement
+   expected = ["The Cruiser is 3 units long", "The Sumbarine is 2 units long"]
+   assert_equal expected, @ui.prompt_ship_placement
+  end
+
+  def test_place_ship
+    assert_equal true, @ui.place_ship(@user_ships[0], ["A1", "A2", "A3"])
+    assert_equal false, @ui.place_ship(@user_ships[0], ["A1", "A2", "A5"])
+  end
+  
 end
